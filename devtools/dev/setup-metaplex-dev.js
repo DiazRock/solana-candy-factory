22a76c3f8f7e4c6f0a71cec3aa03e5ff3c25d9b0
DiazRock <DiazRock>
2021-09-28 14:44:46 -0400

    Metaplex setup for candy-machine

--- devtools/dev/setup-metaplex-dev.js
+++ devtools/dev/setup-metaplex-dev.js
@@ -21,7 +21,7 @@ async function getStartDate() {
     output: process.stdout
   });
   return new Promise((resolve) => {
-    rl.question("Type start date of the minting [default 16 Sep 2021 00:00:00]: ", function (value) {
+    rl.question("Type start date of the minting [default 24 Sep 2021 00:00:00]: ", function (value) {
       rl.close();
       resolve(value);
     });
@@ -29,17 +29,19 @@ async function getStartDate() {
 }
 
 async function setupMetaplexDev() {
+  console.log("En la función metaplex ")
   const file = fs.readFileSync('./devtools/dev/setup-metaplex-dev.sh')
   const fileContent = file.toString()
   const splittedContent = fileContent.split('\n')
   const mintPrice = await getMintPrice()
-  splittedContent[5] = `metaplex create_candy_machine --env devnet --keypair ~/.config/solana/candyfactory-devnet.json --price ${mintPrice || 1} > ./logs/dev/candy-machine-log.txt`
+  splittedContent[5] = `metaplex create_candy_machine --env devnet --keypair /home/diazrock/.config/solana/devnet.json --price ${mintPrice || 1} > ./logs/dev/candy-machine-log.txt`
 
   const startDate = await getStartDate()
-  splittedContent[7] = `metaplex update_candy_machine -d "${startDate || '16 Sep 2021 00:00:00'}" --env devnet --keypair ~/.config/solana/candyfactory-devnet.json > ./logs/dev/candy-machine-start-date.txt`
+  splittedContent[7] = `metaplex update_candy_machine -d "${startDate || '24 Sep 2021 00:00:00'}" --env devnet --keypair /home/diazrock/.config/solana/devnet.json > ./logs/dev/candy-machine-start-date.txt`
 
   fs.unlinkSync('./devtools/dev/setup-metaplex-dev.sh')
   fs.writeFileSync('./devtools/dev/setup-metaplex-dev.sh', splittedContent.join(`\n`))
+  
   shelljs.exec('sh ./devtools/dev/setup-metaplex-dev.sh')
 }
 
